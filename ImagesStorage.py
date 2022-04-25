c54cf24f35c9ce87a7bbd09aff78c22682a1fa5c
Adrae-Alaoui <104110498+Adrae-Alaoui@users.noreply.github.com>
2022-04-25 13:43:22 +0200

    Update ImagesStorage.py

--- ImagesStorage.py
+++ ImagesStorage.py
@@ -1,9 +1,22 @@
 from minio import Minio
-from dotenv import load_dotenv
 from minio.error import S3Error
-
+import os
+import psycopg2
 
 access_key= "gamma"
 secret_key= "1e56abe075d2912c9358dab75c12c9a1"
-client = Minio("minio.dexor.de", access_key=access_key, secret_key=secret_key, region="eu",secure=True)
-client.make_bucket("photos")
+client = Minio("minio.dexor.de", access_key=access_key, secret_key=secret_key, region="eu")
+
+con = psycopg2.connect(
+    name= "gamma_photos",
+    user= "gamma",
+    password= "8ebe63401e50bb351f508d4480753cad",
+    host= "db.dexor.de",
+    port= 5432)
+
+
+for file in os.listdir():
+    if '.json' in file:
+    client.put_object("photos", "gamma", file)
+
+con.close()
